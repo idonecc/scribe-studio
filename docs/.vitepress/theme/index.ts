805a963d5d0c3142b021001e6d84eb5310914fbd
autogame-17 <17@evomap.ai>
2026-05-12 17:48:26 +0800

    Squashed 'backend/core/' content from commit 2fa5e24
    git-subtree-dir: backend/core
    git-subtree-split: 2fa5e24d8f60097de3586cf8584fc29efcb96a17

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import DefaultTheme from 'vitepress/theme'
+
+import DownloadButton from '../components/DownloadButton.vue'
+import EnvInfo from '../components/EnvInfo.vue'
+import LatestIssues from '../components/LatestIssues.vue'
+import ScalarApiReference from './components/ScalarApiReference.vue'
+import SponsorList from '../components/SponsorList.vue'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('DownloadButton', DownloadButton)
+    app.component('EnvInfo', EnvInfo)
+    app.component('LatestIssues', LatestIssues)
+    app.component('ScalarApiReference', ScalarApiReference)
+    app.component('SponsorList', SponsorList)
+  }
+}
+
